5296d2c924e9b46ea740e2823822d2c168d5fcab
Elif <elffemre@gmail.com>
2022-07-11 19:20:40 +0300

    v.0.1.4 - eski kodlar değiştirildi
    -input text kullanıldı
    -if kaldırıldı
    -required eklendi

--- Data/Grocery.cs
+++ Data/Grocery.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 namespace Blazor_App.Data
+
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15,ErrorMessage = "Name should be less then 15 characters.")]
+
         public string Name { get; set; }
+        [Required]
+        [Range(1,10000,ErrorMessage = "valid price range is (1-10000")]
         public float Price { get; set; }
     }
 }
